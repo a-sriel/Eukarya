586b088345cdd083bafdade48c3cdc94572b0c41
Lcmiller411 <lcm6402@gmail.com>
2026-03-20 20:54:55 -0500

    Add Consume mechanic to be attached to the player

---
+++ Assets/Scripts/Game Mechanics/ConsumeAbility.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script should be attached to the player and all involved objects need the Health script as well
+public class ConsumeAbility : MonoBehaviour
+{
+    // to detect potential consume targets
+    public float consumeRange = 2f;
+    public float healthGainOnConsume = 20f;
+
+    void Update()
+    {
+        // Check for player input to initiate consume (e.g., "E" key or a button)
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TryConsume();
+        }
+    }
+
+    private void TryConsume()
+    {
+        // Use Physics.OverlapSphere (or BoxCast, etc. depending on your game type)
+        // to find enemies within the consume range
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, consumeRange);
+
+        foreach (var hitCollider in hitColliders)
+        {
+            // Check if the detected object has the "Enemy" tag and is dead
+            if (hitCollider.CompareTag("Prey"))
+            {
+                Health enemyHealth = hitCollider.GetComponent<Health>();
+
+                if (enemyHealth != null && enemyHealth.IsDead())
+                {
+                    // Perform the consumption
+                    Consume(enemyHealth);
+                    return; // Consume one enemy at a time
+                }
+            }
+        }
+    }
+
+    private void Consume(Health consumedEnemyHealth)
+    {
+
+        // Remove the consumed enemy from the scene
+        Destroy(consumedEnemyHealth.gameObject);
+        Debug.Log("Enemy consumed! Gained " + healthGainOnConsume + " health.");
+
+        // Add any other effects (visuals, sound, ability unlock, etc.)
+    }
+}
