46053ff18b2f3d3f4da6d5eb6ae454a6d8b2b547
Lcmiller411 <lcm6402@gmail.com>
2026-04-29 15:04:42 -0500

    Stamina update section2
    This branch has edits I made to the player controller to allow it to use stamina for sprinting.

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -5,22 +5,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private AnimationController animationController;
-
     private Rigidbody rb;
     private float movementX;
     private float movementY;
 
     // Player's movement speed
-    private float speed;
-    public float walkSpeed = 15;
+    public float speed = 0;
+    private float walkSpeed;
     private float sprintSpeed = 0;
 
     // Stats for swimming scenes
     private Vector3 floating;
     public float floatForce = 2.0f;
 
-    bool stopMoving = false;
+    // Stamina Reference
+    [SerializeField] private StaminaBarManager staminaScript;
+
 
     // Start is called before the first frame update
     void Start()
@@ -28,13 +28,10 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
 
         // Store inputted speed
-        speed = walkSpeed;
-        sprintSpeed = walkSpeed * 2.5f;
+        walkSpeed = speed;
+        sprintSpeed = walkSpeed * 1.5f;
 
         floating = new Vector3(0.0f, floatForce, 0.0f);
-        rb.useGravity = true;
-
-        animationController = gameObject.GetComponent<AnimationController>();
     }
 
     // Called when move input detected
@@ -80,55 +77,37 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        stopMoving = animationController.specialAnimationPlaying();
-
-        if (stopMoving)
-            speed = 0;
-        else
+        // Sprinting handling (Shift)
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift) && staminaScript.CanSprint())
         {
-            // Sprinting handling (Shift)
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) ||
+                Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+            {
+                speed = sprintSpeed;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+        {
+            speed = walkSpeed;
+        }
+
+        // Floating handling for swimming (Spacebar)
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Physics.gravity, ForceMode.Acceleration);
+            rb.AddForce(floating * 0.25f, ForceMode.Impulse);
+        }
+
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) &&
+                !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+        {
+            // Sinking handling (Shift with no keypress)
             if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
             {
-                if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) ||
-                    Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
-                {
-                    print("SPRINTING");
-                    speed = sprintSpeed;
-                }
-            }
-
-            // if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
-            else
-            {
-                speed = walkSpeed;
-            }
-        }
-
-        if (enableFloating)
-        {
-            // Floating handling for swimming (Spacebar)
-            if (Input.GetKey(KeyCode.Space))
-            {
                 rb.AddForce(Physics.gravity, ForceMode.Acceleration);
-                rb.AddForce(floating * 0.25f, ForceMode.Impulse);
-            }
-
-            if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) &&
-                    !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
-            {
-                // Sinking handling (Shift with no keypress)
-                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-                {
-                    rb.AddForce(Physics.gravity, ForceMode.Acceleration);
-                    rb.AddForce(floating * -10.25f, ForceMode.Force);
-                }
+                rb.AddForce(floating * -10.25f, ForceMode.Force);
             }
         }
-
-    }
-
-    public void enableSwimming(bool flag)
-    {
-        enableFloating = flag;
     }
 }
