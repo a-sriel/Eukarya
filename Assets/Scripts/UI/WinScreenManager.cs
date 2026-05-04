a91540d0c1ec234922a0c772cb96051159ccf00b
Drakatoa <ragoel123@gmail.com>
2026-05-03 20:53:05 -0500

    Fix: Win screen

---
+++ Assets/Scripts/UI/WinScreenManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreenManager : MonoBehaviour
+{
+    public GameObject winScreen;
+    private bool shown = false;
+
+    public void Show()
+    {
+        if (shown) return;
+        shown = true;
+        winScreen.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        if (MusicManager.Instance != null)
+            MusicManager.Instance.StopMusic();
+    }
+
+    public void goToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        if (MusicManager.Instance != null)
+            MusicManager.Instance.StopMusic();
+    }
+}
